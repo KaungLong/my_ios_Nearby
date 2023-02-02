5d1cfe36046d229521a2e609c3bf55d0e1ce1395
WeiMoKaungLong <infinity19971023@gmail.com>
2023-02-02 16:58:25 +0800

    Main ver_1.02
    #更新：顯示搜尋結果於conslut

--- NearBy/ViewController.swift
+++ NearBy/ViewController.swift
@@ -4,121 +4,136 @@
 //
 //  Created by 危末狂龍 on 2023/2/2.
 //
+
 import UIKit
 import MapKit
 
-class ViewController: UIViewController  {
-    
-    var locationManager:CLLocationManager?
-    
-    lazy var mapView:MKMapView = {
+class ViewController: UIViewController {
+
+    var locationManager: CLLocationManager?
+
+    lazy var mapView: MKMapView = {
         let map = MKMapView()
         map.showsUserLocation = true
         map.translatesAutoresizingMaskIntoConstraints = false
         return map
     }()
-    lazy var  searchTextField:UITextField = {
+    
+    lazy var searchTextField: UITextField = {
         let searchTextField = UITextField()
-        searchTextField.delegate = self
         searchTextField.layer.cornerRadius = 10
+        searchTextField.delegate = self
         searchTextField.clipsToBounds = true
         searchTextField.backgroundColor = UIColor.white
-        searchTextField.placeholder = "搜尋"
+        searchTextField.placeholder = "Search"
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
         searchTextField.leftViewMode = .always
         searchTextField.translatesAutoresizingMaskIntoConstraints = false
         return searchTextField
+        
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //initialize location manager
+        // initialize location manager
         locationManager = CLLocationManager()
         locationManager?.delegate = self
+        
         locationManager?.requestWhenInUseAuthorization()
-        locationManager?.requestLocation()
         locationManager?.requestAlwaysAuthorization()
+        locationManager?.requestLocation()
         
         setupUI()
     }
+    
+    private func setupUI() {
+        
+        view.addSubview(searchTextField)
+        view.addSubview(mapView)
+        
+        view.bringSubviewToFront(searchTextField)
+        
+        // add constraints to search text field
+        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        searchTextField.returnKeyType = .go
+        
+        // add constraints to the mapView
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
     private func checkLocationAuthorization() {
-        guard let locationManager = locationManager ,
-            let location = locationManager.location else {
-            return
-        }
+        guard let locationManager = locationManager,
+              let location = locationManager.location else { return }
+        
         switch locationManager.authorizationStatus {
-        case.authorizedAlways,.authorizedWhenInUse:
-            let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 750, longitudinalMeters: 750)
-            mapView.setRegion(region, animated: true)
-        case .denied:
-            print("Location services has been denied.")
-        case .notDetermined, .restricted:
-            print("Location cannot be determined or restricted.")
-        @unknown default:
-            print("Unknown error. Unable to get location.")
+            case .authorizedWhenInUse, .authorizedAlways:
+                let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 750, longitudinalMeters: 750)
+                mapView.setRegion(region, animated: true)
+            case .denied:
+                print("Location services has been denied.")
+            case .notDetermined, .restricted:
+                print("Location cannot be determined or restricted.")
+            @unknown default:
+                print("Unknown error. Unable to get location.")
         }
     }
-    private func findNearbyPlaces(by query:String) {
-         
-        //clear all annotations
-        mapView.removeAnnotation(mapView.annotations as! MKAnnotation)
+    
+    private func findNearbyPlaces(by query: String) {
+        
+        // clear all annotations
+        mapView.removeAnnotations(mapView.annotations)
         
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = query
         request.region = mapView.region
         
         let search = MKLocalSearch(request: request)
-        search.start { response,error in
-            guard let response = response, error == nil else {return}
+        search.start { response, error in
             
+            guard let response = response, error == nil else { return }
             print(response.mapItems)
         }
         
-        
     }
-    
-    private func setupUI(){
-        view.addSubview(searchTextField)
-        view.addSubview(mapView)
-        view.bringSubviewToFront(searchTextField)
-        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2).isActive = true
-        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
-        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        searchTextField.topAnchor.constraint(equalTo: view.topAnchor,constant: 60).isActive = true
-        searchTextField.returnKeyType = .go
-        
-        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
-        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
-    }
-
 }
 
-extension ViewController:UITextFieldDelegate{
+
+extension ViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         let text = textField.text ?? ""
-        if !text.isEmpty{
+        if !text.isEmpty {
             textField.resignFirstResponder()
-            //find near place
+            // find nearby places
             findNearbyPlaces(by: text)
         }
         
-     return true
+        return true
     }
+    
 }
 
-extension ViewController:CLLocationManagerDelegate{
+extension ViewController: CLLocationManagerDelegate {
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
     }
+    
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         checkLocationAuthorization()
     }
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("error")
+        print(error)
     }
+    
 }
+
