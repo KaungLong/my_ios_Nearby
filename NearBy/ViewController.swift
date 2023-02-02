dc655a0d803f51ed4d04aca4632d2a6cbc33d8b1
WeiMoKaungLong <infinity19971023@gmail.com>
2023-02-02 14:58:03 +0800

    Main 1.00
    #完成基礎地圖佈局

--- NearBy/ViewController.swift
+++ NearBy/ViewController.swift
@@ -4,16 +4,88 @@
 //
 //  Created by 危末狂龍 on 2023/2/2.
 //
-
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
+    
+    var locationManager:CLLocationManager?
+    
+    lazy var mapView:MKMapView = {
+        let map = MKMapView()
+        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    lazy var  searchTextField:UITextField = {
+        let searchTextField = UITextField()
+        searchTextField.layer.cornerRadius = 10
+        searchTextField.clipsToBounds = true
+        searchTextField.backgroundColor = UIColor.white
+        searchTextField.placeholder = "搜尋"
+        searchTextField.leftViewMode = .always
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //initialize location manager
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.requestLocation()
+        locationManager?.requestAlwaysAuthorization()
+        
+        setupUI()
     }
+    private func checkLocationAuthorization() {
+        guard let locationManager = locationManager ,
+            let location = locationManager.location else {
+            return
+        }
+        switch locationManager.authorizationStatus {
+        case.authorizedAlways,.authorizedWhenInUse:
+            let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 750, longitudinalMeters: 750)
+            mapView.setRegion(region, animated: true)
+        case .denied:
+            print("Location services has been denied.")
+        case .notDetermined, .restricted:
+            print("Location cannot be determined or restricted.")
+        @unknown default:
+            print("Unknown error. Unable to get location.")
+        }
 
+    }
+    
+    private func setupUI(){
+        view.addSubview(searchTextField)
+        view.addSubview(mapView)
+        view.bringSubviewToFront(searchTextField)
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2).isActive = true
+        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor,constant: 60).isActive = true
+        searchTextField.returnKeyType = .go
+        
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+    }
 
 }
 
+extension ViewController:CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error")
+    }
+}
